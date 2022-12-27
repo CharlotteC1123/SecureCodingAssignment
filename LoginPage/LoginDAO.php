ed082c21b447fc76e0fafa87e7dc00f3e4d958f2
CharlotteC1123 <60344136+CharlotteC1123@users.noreply.github.com>
2022-12-27 22:00:06 +0000

    Add files via upload

--- LoginPage/LoginDAO.php
+++ LoginPage/LoginDAO.php
@@ -5,9 +5,17 @@ include '../common/php/common.php';
 
 session_start();// starts the session
   
+
+
+
 $Email = $_POST['userName']; //gets the email input 
 $pass = $_POST['pass']; //get the password input
- 
+
+$stmt = $connection->prepare('SELECT count(*)FROM tbl_user WHERE email = ?'); //secures the query to prevent sql injection 
+$stmt->bind_param('s', $Email); 
+$stmt->execute(); 
+$result = $stmt->get_result(); 
+
 $query = "SELECT * FROM tbl_user WHERE email='".$Email."'"; // finds row with the email mataching user email
 $tbl = mysqli_query($connection,$query);
 if(mysqli_num_rows($tbl)>0){//if there is an email in the database same as what was entered
